35630bd158acfdd2c3af2d0b87f4043e9a7baec7
takeru20 <t20vinci@gmail.com>
2022-01-10 12:40:40 +0900

    Userモデルのテストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, presence: true
+  validates :nickname,               presence: true
+  validates :encrypted_password,     presence: true, length: { minimum: 6 }
+  validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday,               presence: true
 end
