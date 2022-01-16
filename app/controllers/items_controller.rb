94767067d3cf04d75af5853b3fe63931d43f72e1
takeru20 <t20vinci@gmail.com>
2022-01-16 16:54:54 +0900

    商品出品ページ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :image).merge(user_id: current_user.id)
+  end
+
 end
