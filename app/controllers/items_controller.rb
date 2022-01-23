e12fe141be0a6bf667774f9b93aab965cfb2aa90
takeru20 <t20vinci@gmail.com>
2022-01-23 11:22:04 +0900

    コードレビュー修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
-    @products = Product.all
   end
 
   def new
--- app/models/product.rb
+++ app/models/product.rb
@@ -17,6 +17,6 @@ class Product < ApplicationRecord
   validates :shipping_fee_id,      numericality: { other_than: 1, message: "can't be blank" }
   validates :area_id,              numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_date_id,     numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,                numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+  validates :price,                numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
                                    format: { with: /\A[0-9]+\z/ }
 end
