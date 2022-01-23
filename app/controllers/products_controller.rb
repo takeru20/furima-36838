a8f389ddec44d752f876eb99814b68905ca4a15b
takeru20 <t20vinci@gmail.com>
2022-01-23 17:26:40 +0900

    エラー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,22 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    unless @product.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to root_path
+    else
+      render "items/edit"
+    end
+  end
+
   private
 
   def product_params
