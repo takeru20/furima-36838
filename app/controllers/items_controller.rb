7de2916da677cbef1bdc302c58f4271c29388592
takeru20 <t20vinci@gmail.com>
2022-01-23 19:23:29 +0900

    コードレビュー修正⑤

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
   def update
     if @product.update(product_params)
-      redirect_to item_path(product.id)
+      redirect_to item_path(@product.id)
     else
       render :edit
     end
