31760d1a1798aab86745e10f63e8250146e995b3
takeru20 <t20vinci@gmail.com>
2022-01-30 18:12:32 +0900

    ルーティング追記

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
   resources :users
-  resources :products
+  resources :products do
+    resources :orders, only: [:index, :new, :create]
+  end
 end
