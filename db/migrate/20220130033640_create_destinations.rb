0ac325c811f7819a2fa251ebe94ec428767d2854
takeru20 <t20vinci@gmail.com>
2022-01-30 18:10:54 +0900

    Destinationモデルの作成

---
+++ db/migrate/20220130033640_create_destinations.rb
@@ -0,0 +1,14 @@
+class CreateDestinations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :destinations do |t|
+      t.string     :post_code,        null: false
+      t.integer    :area_id,          null: false
+      t.string     :municipalities,   null: false 
+      t.string     :address,          null: false 
+      t.string     :building,
+      t.string     :telephone_number, null: false
+      t.references :order,            null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
