b4a2e6d9df3c38dd6ea179acc22c0a0c13994c48
takeru20 <t20vinci@gmail.com>
2022-02-11 12:29:12 +0900

    修正

--- db/migrate/20220130033640_create_destinations.rb
+++ db/migrate/20220130033640_create_destinations.rb
@@ -5,7 +5,7 @@ class CreateDestinations < ActiveRecord::Migration[6.0]
       t.integer    :area_id,          null: false
       t.string     :municipalities,   null: false 
       t.string     :address,          null: false 
-      t.string     :building,
+      t.string     :building
       t.string     :telephone_number, null: false
       t.references :order,            null: false, foreign_key: true
       t.timestamps
